ec5e4d64de9d35f78d62e93eabf13ad329b2297f
Op_Bshaar <bashaarabdalla5@gmail.com>
2026-06-17 19:42:33 +0300

    .

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")), 
+    path("", include("apps.accounts.urls")),
 ]
 from .views import generate_itinerary
 urlpatterns += [
