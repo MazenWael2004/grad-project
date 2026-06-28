6a908c090aef423db30aeae6cc3d52413d9f82e2
omerym <omerym10@gmail.com>
2026-06-28 20:49:56 +0300

    jwt in fast api

--- Models_Server/app/api/routes.py
+++ Models_Server/app/api/routes.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-router = APIRouter()
+from app.api.dependencies import get_current_user
+
+router = APIRouter(
+    prefix="/api/models",
+    tags=["API"],
+)
 
 
 @router.get("/test")
 def test():
     return {
         "message": "FastAPI is working!"
+    }
+@router.get("/test-protected")
+def test(user=Depends(get_current_user)):
+    return {
+        "message": "Authenticated!",
+        "user": user,
     }
--- Models_Server/app/core/config.py
+++ Models_Server/app/core/config.py
@@ -6,3 +6,5 @@ load_dotenv()
 HOST = os.getenv("HOST", "127.0.0.1")
 PORT = int(os.getenv("PORT", 8000))
 DEBUG = os.getenv("DEBUG", "True") == "True"
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
